62f7ccbcac53ac252228bf198e0cd6df341034f0
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-03-17 10:21:29 -0500

    mantine setup

--- frontend-ecommerce/src/App.jsx
+++ frontend-ecommerce/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@mantine/core/styles.css";
+import { MantineProvider, Button, Title } from "@mantine/core";
+import { theme } from "./theme";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+const App = () => (
+  <MantineProvider theme={theme}>
+    <Title>xd</Title>
+    <Button fullWidth color="red.6">
+      wow
+    </Button>
+  </MantineProvider>
+);
+export default App;
---
+++ frontend-ecommerce/src/theme.js
@@ -0,0 +1,22 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  fontFamily: "Open Sans, sans-serif",
+  cursorType: "pointer",
+  primaryColor: "red",
+  autoContrast: true,
+  colors: {
+    primary: [
+      "#ffe9e9",
+      "#ffd1d1",
+      "#fba0a1",
+      "#f76d6d",
+      "#f34141",
+      "#f22625",
+      "#f21616",
+      "#d8070b",
+      "#c10008",
+      "#a90003",
+    ],
+  },
+});
