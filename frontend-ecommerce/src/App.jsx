2919a80fff9f0d5da77a46134512a7c0449ea9ab
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-03-17 18:41:31 -0500

    refactor

--- frontend-ecommerce/src/App.jsx
+++ frontend-ecommerce/src/App.jsx
@@ -1,15 +1,12 @@
 import "@mantine/core/styles.css";
 import "./App.css";
-import { MantineProvider, BackgroundImage } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import Login from "./Login.jsx";
-import backgroundImage from "./assets/loginBackground.jpg";
 
 const App = () => (
   <MantineProvider theme={theme} defaultColorScheme="dark">
-    <BackgroundImage src={backgroundImage}>
-      <Login />
-    </BackgroundImage>
+    <Login />
   </MantineProvider>
 );
 export default App;
--- frontend-ecommerce/src/Login.jsx
+++ frontend-ecommerce/src/Login.jsx
@@ -8,9 +8,11 @@ import {
   Title,
   Space,
   Paper,
+  BackgroundImage,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import "./App.css";
+import backgroundImage from "./assets/loginBackground.jpg";
 
 const Login = () => {
   const form = useForm({
@@ -29,57 +31,59 @@ const Login = () => {
   });
 
   return (
-    <Box
-      style={{
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        radius: "xs",
-        p: "xl",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Paper className="pager" shadow="xl" radius="xs" p="xl" m="xl">
-        <Box>
-          <Title>Welcome back</Title>
-          <Divider label="Login" labelPosition="left" />
-          <Space h="lg" />
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <TextInput
-              withAsterisk
-              label="Email"
-              placeholder="your@email.com"
-              {...form.getInputProps("email")}
-            />
+    <BackgroundImage src={backgroundImage}>
+      <Box
+        style={{
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          radius: "xs",
+          p: "xl",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper className="pager" shadow="xl" radius="xs" p="xl" m="xl">
+          <Box>
+            <Title>Welcome back</Title>
+            <Divider label="Login" labelPosition="left" />
+            <Space h="lg" />
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+              <TextInput
+                withAsterisk
+                label="Email"
+                placeholder="your@email.com"
+                {...form.getInputProps("email")}
+              />
 
-            <Space h="sm" />
+              <Space h="sm" />
 
-            <TextInput
-              withAsterisk
-              label="Password"
-              placeholder="yourPassword1234"
-              {...form.getInputProps("password")}
-            />
+              <TextInput
+                withAsterisk
+                label="Password"
+                placeholder="yourPassword1234"
+                {...form.getInputProps("password")}
+              />
 
-            <Checkbox
-              color="red.9"
-              mt="md"
-              label="I agree to sell my privacy"
-              {...form.getInputProps("termsOfService", { type: "checkbox" })}
-            />
+              <Checkbox
+                color="red.9"
+                mt="md"
+                label="I agree to sell my privacy"
+                {...form.getInputProps("termsOfService", { type: "checkbox" })}
+              />
 
-            <Group justify="flex-end" mt="md">
-              <Button type="submit" variant="gradient">
-                Submit
-              </Button>
-            </Group>
-          </form>
-        </Box>
-      </Paper>
-    </Box>
+              <Group justify="flex-end" mt="md">
+                <Button type="submit" variant="gradient">
+                  Submit
+                </Button>
+              </Group>
+            </form>
+          </Box>
+        </Paper>
+      </Box>
+    </BackgroundImage>
   );
 };
 
