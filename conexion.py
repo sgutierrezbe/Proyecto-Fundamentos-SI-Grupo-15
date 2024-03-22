2264752965cef601a03edf1450cadc72974a8770
Brando Montoya <baldo542@gmail.com>
2024-03-22 16:29:46 -0500

    consulta login

--- conexion.py
+++ conexion.py
@@ -3,14 +3,20 @@ import mysql.connector as mysql #pip install mysql-connector-python
 db = mysql.connect(user = "root", password = "", host = "localhost", database = "si_ecommerce") #conexion con bd
 cursor = db.cursor()
 
-
-cursor.execute("SHOW DATABASES") 
-dbs = cursor.fetchall()
-for i in dbs:
+columnas = "SHOW COLUMNS FROM usuario"
+cursor.execute(columnas)
+columnas = cursor.fetchall()
+for i in columnas:
     print(i)
+print("")
 
-admin = "SELECT * FROM usuario WHERE rol = 'admin'"
-cursor.execute(admin)
-admin = cursor.fetchall()
-for i in admin:
-    print(i)
+#login
+usuario = "SELECT idusuario, nombre, correo, rol, direccion FROM usuario WHERE correo = %s AND password = %s"
+correo = "admin@gmail.com" #se cambia por el input del form de login
+password = "admin123"
+cursor.execute(usuario,(correo,password))
+usuario = cursor.fetchall()
+print(usuario)
+
+cursor.close()
+db.close()
