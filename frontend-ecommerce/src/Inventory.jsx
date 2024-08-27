709c00e42a3518b68dcd59b52db51479fc757719
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-08-27 13:44:21 -0500

    partially fixed responsiveness of main content in inventory

--- frontend-ecommerce/src/Inventory.jsx
+++ frontend-ecommerce/src/Inventory.jsx
@@ -16,9 +16,11 @@ import "./Inventory.css";
 import IconInventory from "./SvgComponent.jsx";
 import InventoryProduct from "./InventoryProduct.jsx";
 import InventoryFilters from "./InventoryFilters.jsx";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Inventory = () => {
   const [opened, { toggle }] = useDisclosure();
+  const isSmallScreen = useMediaQuery("(max-width: 650px)");
 
   return (
     <AppShell
@@ -51,16 +53,19 @@ const Inventory = () => {
       </AppShell.Navbar>
 
       <AppShell.Main style={{ backgroundColor: "#131314" }}>
-        <SimpleGrid cols={1}>
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
-          <InventoryProduct />
+        <SimpleGrid
+          cols={1}
+          style={
+            isSmallScreen ? { marginBottom: "2rem" } : { marginBottom: "2rem" }
+          }
+        >
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
+          <InventoryProduct isSmallScreen={isSmallScreen} />
         </SimpleGrid>
       </AppShell.Main>
 
--- frontend-ecommerce/src/InventoryProduct.jsx
+++ frontend-ecommerce/src/InventoryProduct.jsx
@@ -9,42 +9,81 @@ import {
 } from "@mantine/core";
 import placeholderImage from "./assets/graphic-card.png";
 
-function InventoryProduct() {
+function InventoryProduct({ isSmallScreen }) {
   return (
     <Paper
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "left",
-        height: "110px",
-      }}
+      style={
+        isSmallScreen
+          ? {
+              display: "flex",
+              flexDirection: "column",
+              overflow: "hidden",
+              maxHeight: "400px",
+            }
+          : { display: "flex", alignItems: "center", height: "100%" }
+      }
     >
       <Image
         src={null}
         fallbackSrc={placeholderImage}
         radius="md"
-        h="100%"
-        w="20vw"
-        fit="contain"
+        h={isSmallScreen ? "8rem" : "100%"}
+        w={isSmallScreen ? "100%" : "8rem"}
+        fit="scale-down"
         style={{
           backgroundColor: "#ffffff",
           paddingInline: "5px",
           marginInlineEnd: "5px",
         }}
       />
-      <Divider orientation="vertical" size="sm" />
-      <Box h="100%">
-        <Title order={3} style={{ marginInline: "5px" }}>
+      <Divider
+        orientation={isSmallScreen ? "horizontal" : "vertical"}
+        size="sm"
+      />
+      <Box
+        style={
+          isSmallScreen
+            ? {
+                height: "fit-content",
+                width: "100%",
+                padding: "0.75rem",
+              }
+            : {
+                height: "100%",
+                paddingTop: "1rem",
+                paddingBottom: "1rem",
+                overflow: "hidden",
+              }
+        }
+      >
+        <Title
+          order={3}
+          style={
+            isSmallScreen ? { height: "fit-content" } : { marginLeft: "5px" }
+          }
+        >
           GeForce GTX 1050 Mobile 3GB
         </Title>
         <Divider />
-        <List style={{ marginInline: "5px" }}>
+        <List
+          style={
+            isSmallScreen
+              ? { height: "fit-content", paddingTop: "1rem" }
+              : { marginLeft: "5px" }
+          }
+        >
           <List.Item>Vram: 4</List.Item>
           <List.Item>Brand: Nvidia</List.Item>
           <List.Item>Manufacturer: Nvidida</List.Item>
         </List>
       </Box>
-      <Box className="clear-btn">
+      <Box
+        style={
+          isSmallScreen
+            ? { height: "fit-content", width: "100%", padding: "1rem" }
+            : { marginLeft: "auto", marginRight: "1rem" }
+        }
+      >
         <NumberInput
           value={1}
           clampBehavior="strict"
